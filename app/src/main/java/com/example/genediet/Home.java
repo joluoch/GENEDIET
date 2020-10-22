c08d90e64836bb35da304cb839167ac1d62877d5
jakin <jakindaodhiambo@gmail.com>
2020-10-22 23:14:47 +0300

    Userprofile

--- app/src/main/java/com/example/genediet/Home.java
+++ app/src/main/java/com/example/genediet/Home.java
@@ -2,8 +2,11 @@ package com.example.genediet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,11 +19,11 @@ public class Home extends AppCompatActivity {
     FirebaseUser mFirebaseUser;
     FirebaseAuth.AuthStateListener mAuthStateListener;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
 
         /*mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -29,7 +32,7 @@ public class Home extends AppCompatActivity {
         }*/
 
 
-        btnLogout = (Button) findViewById(R.id.logout);
+       /* btnLogout = (Button) findViewById(R.id.logout);
         btnSettings = (Button) findViewById(R.id.settings);
 
         btnSettings.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +50,40 @@ public class Home extends AppCompatActivity {
                 Intent intToMain = new Intent(Home.this, Login.class);
                 startActivity(intToMain);
             }
-        });
+        });*/
+
+
+
     }
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.layout.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.registerMenu:{
+                startActivity(new Intent(Home.this, settings.class));
+                break;
+            }
+            case R.id.profileMenu:
+                startActivity(new Intent(Home.this, Userprofile.class));
+                break;
+            case R.id.logoutMenu:
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(Home.this, Login.class);
+                startActivity(intToMain);
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
 
