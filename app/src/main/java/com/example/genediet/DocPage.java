2a488d1917f90d7db49c58031c0cbcca684087ab
jakin <jakindaodhiambo@gmail.com>
2020-12-17 00:21:50 +0300

    saving spinner value to sqlite

--- app/src/main/java/com/example/genediet/DocPage.java
+++ app/src/main/java/com/example/genediet/DocPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,10 +21,14 @@ import com.google.firebase.auth.FirebaseUser;
 public class DocPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spdna1,spdna2,spdna3,spdna4,spdna5,spdnb1,
             spdnb2,spdnb3,spdnb4,spdnb5,spdnc1,spdnc2,spdnc3,spdnc4,spdnc5;
+    String item1,item2,item3,item4,item5,item6,item7,item8,item9,item10,item11,item12,item13,item14,item15;
     Button btnsubmit;
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    SQLiteDatabase sqLiteDatabaseObj;
+    String SQLiteDataBaseQueryHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +56,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
 
         //DNA-A
 
-        spdna1.setOnItemSelectedListener(this);
+        spdna1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item1=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dna2 = ArrayAdapter.createFromResource(
                 this,
@@ -59,7 +75,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dna2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdna2.setAdapter(dna2);
-        spdna2.setOnItemSelectedListener(this);
+        spdna2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item2=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dna3 = ArrayAdapter.createFromResource(
                 this,
@@ -68,7 +94,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dna3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdna3.setAdapter(dna3);
-        spdna3.setOnItemSelectedListener(this);
+        spdna3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item3=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dna4 = ArrayAdapter.createFromResource(
                 this,
@@ -77,7 +113,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dna4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdna4.setAdapter(dna4);
-        spdna4.setOnItemSelectedListener(this);
+        spdna4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item4=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dna5 = ArrayAdapter.createFromResource(
                 this,
@@ -86,7 +132,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dna5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdna5.setAdapter(dna5);
-        spdna5.setOnItemSelectedListener(this);
+        spdna5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item5=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //DNA-A  END
 
@@ -98,7 +154,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnb1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnb1.setAdapter(dnb1);
-        spdnb1.setOnItemSelectedListener(this);
+        spdnb1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item6=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnb2 = ArrayAdapter.createFromResource(
                 this,
@@ -107,7 +173,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnb2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnb2.setAdapter(dnb2);
-        spdnb2.setOnItemSelectedListener(this);
+        spdnb2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item7=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnb3 = ArrayAdapter.createFromResource(
                 this,
@@ -116,7 +192,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnb3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnb3.setAdapter(dnb3);
-        spdnb3.setOnItemSelectedListener(this);
+        spdnb3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item8=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnb4 = ArrayAdapter.createFromResource(
                 this,
@@ -125,7 +211,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnb4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnb4.setAdapter(dnb4);
-        spdnb4.setOnItemSelectedListener(this);
+        spdnb4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item9=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnb5 = ArrayAdapter.createFromResource(
                 this,
@@ -134,7 +230,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnb5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnb5.setAdapter(dnb5);
-        spdnb5.setOnItemSelectedListener(this);
+        spdnb5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item10=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //DNA-B END
 
@@ -146,7 +252,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnc1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnc1.setAdapter(dnc1);
-        spdnc1.setOnItemSelectedListener(this);
+        spdnc1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item11=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnc2 = ArrayAdapter.createFromResource(
                 this,
@@ -155,7 +271,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnc2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnc2.setAdapter(dnc2);
-        spdnc2.setOnItemSelectedListener(this);
+        spdnc2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item12=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnc3 = ArrayAdapter.createFromResource(
                 this,
@@ -164,7 +290,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnc3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnc3.setAdapter(dnc3);
-        spdnc3.setOnItemSelectedListener(this);
+        spdnc3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item13=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnc4 = ArrayAdapter.createFromResource(
                 this,
@@ -173,7 +309,17 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnc4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnc4.setAdapter(dnc4);
-        spdnc4.setOnItemSelectedListener(this);
+        spdnc4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item14=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> dnc5 = ArrayAdapter.createFromResource(
                 this,
@@ -182,11 +328,49 @@ public class DocPage extends AppCompatActivity implements AdapterView.OnItemSele
         );
         dnc5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spdnc5.setAdapter(dnc5);
-        spdnc5.setOnItemSelectedListener(this);
+        spdnc5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item15=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         //DNA-C END
+
+        /*SQLiteDatabase mydatabase = openOrCreateDatabase("DNA",MODE_PRIVATE,null);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS doctors_table(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "ARHGAP31 VARCHAR,BAP1 VARCHAR,BMPR1A VARCHAR,CAVIN1 VARCHAR," +
+                "CEP290 VARCHAR,CHD7 VARCHAR,BCR VARCHAR,BMPR2 VARCHAR," +
+                "CCM2 VARCHAR,CHM VARCHAR,APOB VARCHAR,CEP57 VARCHAR,CLN5 VARCHAR,CLN6 VARCHAR,CLN8 VARCHAR)");*/
+        sqLiteDatabaseObj = openOrCreateDatabase("FoodRec", MODE_PRIVATE, null);
+        sqLiteDatabaseObj.execSQL("CREATE TABLE IF NOT EXISTS doctors_table(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,ARHGAP31 VARCHAR,BAP1 VARCHAR,BMPR1A VARCHAR,CAVIN1 VARCHAR, CEP290 VARCHAR,CHD7 VARCHAR,BCR VARCHAR,BMPR2 VARCHAR,CCM2 VARCHAR,CHM VARCHAR,APOB VARCHAR,CEP57 VARCHAR,CLN5 VARCHAR,CLN6 VARCHAR,CLN8 VARCHAR)");
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InsertData();
+            }
+        });
+    }
+    public void InsertData(){
+        SQLiteDataBaseQueryHolder = "INSERT INTO doctors_table (ARHGAP31,BAP1,BMPR1A,CAVIN1 ,CEP290,CHD7,BCR,BMPR2 , CCM2 ,CHM ,APOB ,CEP57,CLN5,CLN6,CLN8) VALUES('" + item1 + "', " +
+                "'" + item2 + "', '" + item3 + "', '" + item4 + "', '" + item5 + "', '" + item6 + "', '" + item7 + "' , '" + item8 + "', '" + item9 + "', '" + item10 + "', '" + item11 + "'," +
+                " '" + item12 + "', '" + item13 + "',+ '" + item14 + "', '" + item15 + "');";
+
+
+        /*SQLiteDataBaseQueryHolder = "INSERT INTO AndroidJSonTable (ARHGAP31,ARHGAP31,BAP1,BMPR1A,CAVIN1 ,CEP290,CHD7,BCR,BMPR2 ,CCM2 ,CHM ,APOB ,CEP57,CLN5,CLN6,CLN8) VALUES('"+spdna1+"', '"+spdna2+", '"+spdna3+", '"+spdna4+", '"+spdna5+", '"+spdnb1+"" +
+                ", '"+spdnb2+"" + ", '"+spdnb3+", '"+spdnb4+", '"+spdnb5+", '"+spdnc1+", '"+spdnc2+", '"+spdnc3+",+ '"+spdnc4+", '"+spdnc5+"');";
+*/
+        sqLiteDatabaseObj.execSQL(SQLiteDataBaseQueryHolder);
+
+        Toast.makeText(DocPage.this,"Data Inserted Successfully", Toast.LENGTH_LONG).show();
 
 
     }
+
     @SuppressLint("ResourceType")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/genediet/Home.java
+++ app/src/main/java/com/example/genediet/Home.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,16 +13,20 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spprot1,spprot2,spprot3,spst1,spst2,spvit1,spvit2,spvit3,spvit4,spvit5;
+    String Item1,Item2,Item3,Item4,Item5,Item6,Item7,Item8,Item9,Item10;
     Button submit;
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    SQLiteDatabase sqLiteDatabaseObj;
+    String SQLiteDataBaseQueryHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +47,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
 
         ///protein
 
-        spprot1.setOnItemSelectedListener(this);
+        spprot1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item1=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> prot2 = ArrayAdapter.createFromResource(
                 this,
@@ -51,7 +66,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         prot2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spprot2.setAdapter(prot2);
-        spprot2.setOnItemSelectedListener(this);
+        spprot2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item2=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> prot3 = ArrayAdapter.createFromResource(
                 this,
@@ -60,7 +85,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         prot3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spprot3.setAdapter(prot3);
-        spprot3.setOnItemSelectedListener(this);
+        spprot3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item3=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         //end protein
 
         //start starch
@@ -72,7 +107,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         st1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spst1.setAdapter(st1);
-        spst1.setOnItemSelectedListener(this);
+        spst1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item4=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> st2 = ArrayAdapter.createFromResource(
                 this,
@@ -81,7 +126,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         st2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spst2.setAdapter(st2);
-        spst2.setOnItemSelectedListener(this);
+        spst2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item5=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //end starch
 
@@ -94,7 +149,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         vit1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spvit1.setAdapter(vit1);
-        spvit1.setOnItemSelectedListener(this);
+        spvit1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item6=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> vit2 = ArrayAdapter.createFromResource(
                 this,
@@ -103,7 +168,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         vit2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spvit2.setAdapter(vit2);
-        spvit2.setOnItemSelectedListener(this);
+        spvit2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item7=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> vit3 = ArrayAdapter.createFromResource(
                 this,
@@ -112,7 +187,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         vit3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spvit3.setAdapter(vit3);
-        spvit3.setOnItemSelectedListener(this);
+        spvit3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item8=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> vit4 = ArrayAdapter.createFromResource(
                 this,
@@ -121,7 +206,17 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         vit4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spvit4.setAdapter(vit4);
-        spvit4.setOnItemSelectedListener(this);
+        spvit4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item9=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ArrayAdapter<CharSequence> vit5 = ArrayAdapter.createFromResource(
                 this,
@@ -130,9 +225,38 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
         );
         vit5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spvit5.setAdapter(vit5);
-        spvit5.setOnItemSelectedListener(this);
+        spvit5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Item10=String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //end vitamins
+
+        sqLiteDatabaseObj = openOrCreateDatabase("FoodRec", MODE_PRIVATE, null);
+        sqLiteDatabaseObj.execSQL("CREATE TABLE IF NOT EXISTS patients_table(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Glycine_max VARCHAR,Vicia_faba VARCHAR,Trigonella_foenum_graecum VARCHAR,Camellia_sinensis VARCHAR,Cinnamomum_verum VARCHAR,Vitis_vinifera VARCHAR,Laurus_nobilis VARCHAR,Citrus_sinensis VARCHAR,Brassica_oleracea VARCHAR,Origanum_vulgare VARCHAR)");
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InsertData();
+            }
+        });
+
+
+    }
+    public void InsertData(){
+
+        SQLiteDataBaseQueryHolder = "INSERT INTO patients_table(Glycine_max,Vicia_faba,Trigonella_foenum_graecum,Camellia_sinensis,Cinnamomum_verum,Vitis_vinifera,Laurus_nobilis,Citrus_sinensis,Brassica_oleracea ,Origanum_vulgare) VALUES('"+Item1+"', '"+Item2+"', '"+Item3+"', '"+Item4+"', '"+Item5+"', '"+Item6+"','"+Item7+"' , '"+Item8+"', '"+Item9+"', '"+Item10+"');";
+
+        sqLiteDatabaseObj.execSQL(SQLiteDataBaseQueryHolder);
+
+        Toast.makeText(Home.this,"Data Inserted Successfully", Toast.LENGTH_LONG).show();
 
 
     }
@@ -170,6 +294,8 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemSelecte
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
 
     }
 
